7be1f2b8f7b7e58b0639a4c7dd8e1415dd323bcb
Devesh Bhor <deveshbhor@outlook.com>
2019-07-12 13:53:59 -0500

    Initial Commit

---
+++ Splurged/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splurged
+//
+//  Created by Devesh Bhor on 7/12/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
